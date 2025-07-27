35d4e04bd208d113c02f8629cfa1c2d9ce860c7f
Vinícius Ares <vinicius.ares12@gmail.com>
2025-07-27 16:58:58 -0300

    Adição do gitignore, processos, escalonamento FCFS e PDF

---
+++ GerenciamentoProcessos/FCFS.py
@@ -0,0 +1,16 @@
+# FCFS - First Come First Served or FIFO (Simulation)
+
+from processos import processos 
+
+tempo=0
+
+for processo in processos:
+    if tempo <processo["Chegada"]:
+        tempo=processo["Chegada"]
+
+    inicio = tempo
+    fim = tempo + processo["Duração"]
+    espera = inicio - processo["Chegada"]
+
+    print(f"{processo['ID']}: Chegada={processo['Chegada']}, Início={inicio}, Fim={fim}, Espera={espera}")
+    tempo = fim
---
+++ GerenciamentoProcessos/processos.py
@@ -0,0 +1,6 @@
+processos = [
+    {"ID": "P1", "Chegada": 0, "Duração": 8},
+    {"ID": "P2", "Chegada": 2, "Duração": 4},
+    {"ID": "P3", "Chegada": 4, "Duração": 2},
+    {"ID": "P4", "Chegada": 5, "Duração": 3},
+]
